b97685cb0dca17320199e987654ecfb773ba58f7
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-09-24 15:54:23 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,8 +1,11 @@
-function WeatherDisplay(){
-
-  return(
-    <p>Hii</p>
-  )
+function WeatherDisplay({weatherInfo}){
+    const weatherColor=weatherInfo.temperature >20 ?"red":"blue"
+    return(
+        <div>
+            <p>Temperature: <span style={{color:weatherColor}}>{weatherInfo.temperature}</span></p>
+            <p>Conditions: {weatherInfo.conditions}</p>
+        </div>
+    )
 }
 
 export default WeatherDisplay
