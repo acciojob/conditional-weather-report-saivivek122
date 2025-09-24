10792c0e8029b5b7606e704d1fadae725c583ace
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-09-24 16:03:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-     <WeatherDisplay weatherInfo={weather}/>
+     <WeatherDisplay weatherInfo={weather}/> 
     </div>
   )
 }
